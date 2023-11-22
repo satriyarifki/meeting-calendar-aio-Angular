bb9212c4782383c174930a15790279b56132f01c
satriyarifki <rifkisatriya@gmail.com>
2023-11-22 16:58:32 +0700

    fix(navbar) added notif layout

--- src/app/layouts/navbar/navbar.component.ts
+++ src/app/layouts/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 import { AlertType } from 'src/app/services/alert/alert.model';
 import { AlertService } from 'src/app/services/alert/alert.service';
 import { ApiService } from 'src/app/services/api.service';
@@ -11,10 +12,12 @@ import { AuthService } from 'src/app/services/auth/auth.service';
 })
 export class NavbarComponent {
   notifBool:Boolean = false
+  voteNotif:any[] = []
 
-  constructor(private authService:AuthService,private alertService:AlertService,private apiService:ApiService){
+  constructor(private authService:AuthService,private alertService:AlertService,private apiService:ApiService, public router:Router){
     apiService.voteDetailsByUserGet(authService.getUser()[0]?.lg_nik).subscribe(res=>{
       console.log(res);
+      this.voteNotif = res
       
     })
   }
--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  baseApi: 'http://localhost:3666/',
+  baseApi: 'http://localhost:3066/',
   // baseApi: 'http://192.168.9.47:3666/',
   // defaultApi: 'http://localhost:3444/',
   defaultApi: 'http://192.168.9.47:3444/',
